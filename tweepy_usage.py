a4c9f3342d076cf8ea46c7cf02f6e1a5ae58be35
Jithin Madhusudanan S <msjithin108@gmail.com>
2021-10-11 21:39:37 -0500

    gets more than 100 tweets

--- tweepy_usage.py
+++ tweepy_usage.py
@@ -3,14 +3,36 @@ import twitter_api
 import pandas as pd
 import os
 from save_to_csvfile import save_to_csv
-import json
+
+output_file = 'tweets_data.csv'
+
+def unfold_dict(inputList=[] , parameter=None):
+    '''
+    This function takes in a list of dictionaries and returns 
+    keys in that dictionary matching 'parameter'
+    '''
+    out_string = ''
+    for entities in inputList:
+        if parameter in entities:
+            out_string +=  entities[parameter] + ', '
+    return out_string
+
 
 # function to perform data extraction
-def scrape(words, date_until, numtweet):
+def get_tweets(words, date_until, numtweet, last_tweet_id=None):
+    '''
+    This function takes in a word or phrase for query and returns number of tweets = numtweet
+    with 'words' in it
+    This is a problem : 
+    the search index has a 7-day limit. In other words, no tweets will be found for a date older than one week.
+
+    '''
     search_phrase = words
-    tweets = twitter_api.my_api.search_tweets(q=search_phrase, count=numtweet)
+    if not last_tweet_id:
+        tweets = twitter_api.my_api.search_tweets(q=search_phrase, count=numtweet)
+    else:
+        tweets = twitter_api.my_api.search_tweets(q=search_phrase, count=numtweet, max_id=last_tweet_id)
     # detailes in https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
-    out_dict = {}
     data = []
     for t in tweets:
         tmp_dict = {
@@ -28,20 +50,29 @@ def scrape(words, date_until, numtweet):
                     'user_created_at' : t.user.created_at,
                     'statuses_count' : t.user.statuses_count,
                     'location' : t.user.location,
-                    'hashtags' : t.entities['hashtags'],
-                    'symbols'  : t.entities['symbols'],
-                    'user_mentions' : t.entities['user_mentions'],
+                    'hashtags' : unfold_dict(t.entities['hashtags'], 'text'),
+                    'symbols'  : unfold_dict( t.entities['symbols'], 'text'),
+                    'mentioned_user_names' : unfold_dict(t.entities['user_mentions'], 'screen_name'),
+                    'mentioned_user_ids' : unfold_dict(t.entities['user_mentions'],  'id_str'),
                     'source'   :  t.source,
                     'in_reply_to_user_id' : t.in_reply_to_user_id,
         }
         data.append(tmp_dict)    
     # for d in data:
-    #     print("{} ".format(d).encode('cp850', errors='replace'))
+    #     print("{} ".format(d).encode('cp850', errors='replace'))   ##### IMP incase you have encoding issue in python, use this format
     df = pd.DataFrame(data)
-    print(df)
-    output_file = 'out.csv'
+    #print(df)
     header_state = False  if os.path.isfile(output_file) else True
     df.to_csv(output_file , mode='a', header=header_state, index = False)
+
+def get_twitter_data(words, date_until, numtweets):
+    last_tweet_id = None
+    for n in range(int(numtweets/100)):
+        if os.path.isfile(output_file):
+            data = pd.read_csv(output_file)
+            last_tweet_id = data.tweet_id.iloc[-1] -1 
+            print(last_tweet_id + 1)
+        get_tweets(words, date_until, 100, last_tweet_id=last_tweet_id)
 
 
 if __name__ == '__main__':
@@ -51,7 +82,7 @@ if __name__ == '__main__':
     # Enter Date since The Tweets are required in yyyy-mm--dd
     date_until = "2019-01--01"
     # number of tweets you want to extract in one run
-    numtweet = 5
-    scrape(words, date_until, numtweet)
-    #scape_to_file(words, date_until, numtweet)
+    numtweets = 10000
+    #get_tweets(words, date_until, numtweet)
+    get_twitter_data(words, date_until, numtweets)
     print('Scraping has completed!')
