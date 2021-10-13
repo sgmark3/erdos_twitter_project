189289206cbc1b67daa955a62bf5e2b7448f9c38
amcmillan42 <48335839+amcmillan42@users.noreply.github.com>
2021-10-13 09:54:26 -0400

    Update Twitter_Sentiment_Analysis.py

--- Twitter_Sentiment_Analysis/Twitter_Sentiment_Analysis.py
+++ Twitter_Sentiment_Analysis/Twitter_Sentiment_Analysis.py
@@ -1,19 +1,18 @@
-# Establish Sentiment Features
-# 1. Directional indicators
-# a. posWordsHe = word counts of positive words from Henry '08
-# b. negWordsHe = word counts of negative words from Henry '08
-# c. posWordsLM = word counts of positive words from Loughran and McDonald '11
-# d. negWordsLM = word counts of negative words from Loughran and McDonald '11
-# e. posNgrams = word counts of positive ngrams from Hagenau '13
-# d. negNgrams = word counts of negative ngrams from Hagenau '13
-# Train_test split
+#Directional indicators
+    # a. posWordsHe = word counts of positive words from Henry '08
+    # b. negWordsHe = word counts of negative words from Henry '08
+    # c. posWordsLM = word counts of positive words from Loughran and McDonald '11
+    # d. negWordsLM = word counts of negative words from Loughran and McDonald '11
+    # e. posNgrams = word counts of positive ngrams from Hagenau '13
+    # d. negNgrams = word counts of negative ngrams from Hagenau '13
 
 
-class Twitter_Sentiment_Analysis:
+class Twitter_Sentiment_Directional_Indicators:
     """
     This class computes the positive and negative sentiments associated with
-    tweet data taken as input. The scoring is performed using the libraries of
-    Henry '08, Loughran and McDonald '11, and Hagenau '13.
+    tweet data taken as input for the directional indicators. The scoring is 
+    performed using the libraries of Henry '08, Loughran and McDonald '11, 
+    and Hagenau '13.
     """
 
     def __init__(self, tweet_frame, pos_words, neg_words):
@@ -23,25 +22,38 @@ class Twitter_Sentiment_Analysis:
 
 
     def pos_neg_counter(self, pos_library_name, neg_library_name):
+         """
+          param pos_library_name: The name in the literature for the positive library
+          param neg_library_name: The name in the literature for the negative library
+          return: The twitter dataframe with columns augmented with 
+          positive and negative scores
+         """
+        
         if not isinstance(pos_library_name, str) or not isinstance(neg_library_name, str):
             print('The library names need to be strings.')
         try:
             self.tweet_frame[pos_library_name], self.tweet_frame[neg_library_name] = self.tweet_frame['tweet'].apply(
-                get_word_counts, args=
+                self.get_word_counts, args=
                 (self.pos_words, self.neg_words,))
-    except:
-        print('Positive and Negative words not loaded correctly')
+        except:
+            print('Positive and Negative words not loaded correctly')
+        return self.tweet_frame
 
 
-def get_word_counts(text,self):
-    count_pos = 0
-    count_neg = 0
-    if not isinstance(self.pos_words, list) or not isinstance(self.neg_words, list):
-        print('The positive and negative words are not in list format.')
+    def get_word_counts(text,self):
+        """
+        param self.pos_words: The collection of positive words
+        param self.neg_words: The collection of negative words
+        param text: The text to be analyzed
+        """
+        count_pos = 0
+        count_neg = 0
+        if not isinstance(self.pos_words, list) or not isinstance(self.neg_words, list):
+            print('The positive and negative words are not in list format.')
 
-    for word in text:
-        if word in self.pos_words:
-            count_pos += 1
-        if word in self.neg_words:
-            count_neg += 1
-    return [count_pos, count_neg]
+        for word in text:
+            if word in self.pos_words:
+                count_pos += 1
+            if word in self.neg_words:
+                count_neg += 1
+        return [count_pos, count_neg]
