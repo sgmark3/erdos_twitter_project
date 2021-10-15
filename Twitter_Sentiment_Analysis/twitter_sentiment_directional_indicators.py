a4cb34428ee94dc24f6f8c376a1d65c3f4b750c3
amcmillan42 <48335839+amcmillan42@users.noreply.github.com>
2021-10-15 09:09:08 -0400

    Update twitter_sentiment_directional_indicators.py

--- Twitter_Sentiment_Analysis/twitter_sentiment_directional_indicators.py
+++ Twitter_Sentiment_Analysis/twitter_sentiment_directional_indicators.py
@@ -1,10 +1,10 @@
 #Directional indicators
-    # a. posWordsHe = word counts of positive words from Henry '08
-    # b. negWordsHe = word counts of negative words from Henry '08
-    # c. posWordsLM = word counts of positive words from Loughran and McDonald '11
-    # d. negWordsLM = word counts of negative words from Loughran and McDonald '11
-    # e. posNgrams = word counts of positive ngrams from Hagenau '13
-    # d. negNgrams = word counts of negative ngrams from Hagenau '13
+    # a. posWordsHe = word counts of positive words from Henry '08 (File already uploaded AM)
+    # b. negWordsHe = word counts of negative words from Henry '08 (File already uploaded AM)
+    # c. posWordsLM = word counts of positive words from Loughran and McDonald '11 (Still needed)
+    # d. negWordsLM = word counts of negative words from Loughran and McDonald '11 (Still needed)
+    # e. posNgrams = word counts of positive ngrams from Hagenau '13 (Still needed)
+    # d. negNgrams = word counts of negative ngrams from Hagenau '13 (Still needed)
 
 
 class twitter_sentiment_directional_indicators:
