ca84e48413dbb84dafa245480d333497fb45a38e
Jithin Madhusudanan S <msjithin108@gmail.com>
2021-10-22 22:46:40 -0500

    adds loop over snp500 companies

---
+++ get_snp500_tweets.py
@@ -0,0 +1,20 @@
+from twitter_to_csv import get_twitter_data
+
+import pandas as pd
+
+
+def main():
+
+    snp500_df = pd.read_csv("data\Stock_indices\snp500_list.csv")
+    #print(snp500_df.head())
+    # for i in range(len(snp500_df)):
+    # 	print("ticker ", snp500_df['Symbol'].iloc[i], " company ", snp500_df['Security'].iloc[i])
+    query = 'snp500'         # now define number of total tweets you want, should be greater than max_results_per_query
+    number_of_tweets = 500  # now define output file name
+    filename = 'df_'+query
+    print(query, number_of_tweets, filename)
+    get_twitter_data(query=query, numtweets=number_of_tweets, output_file=filename)
+
+
+if __name__ == "__main__":
+    main()
