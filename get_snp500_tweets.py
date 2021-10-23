bb186dd382f2a568e9e014436708d118c4f9f66d
Jithin Madhusudanan S <msjithin108@gmail.com>
2021-10-23 13:47:24 -0500

    adds ticker length condition

--- get_snp500_tweets.py
+++ get_snp500_tweets.py
@@ -14,10 +14,13 @@ def main():
         ticker = snp500_df['Symbol'].iloc[i]
         name = snp500_df['Security'].iloc[i].split(' ')
         name_to_use = name[0]
+        ticker_to_use = "#"+ticker+" OR $"+ticker
+        if len(ticker) > 2 :
+            ticker_to_use += " OR "+ticker
         if len(name[0])<=3 :
             query_file.write("special case ................. \n ")
             name_to_use = ' '.join(name)
-        out_string = "ticker: {t} , company: {n}  ||||||||||| query = #{t} OR ${t} OR ({c}) \n\n".format(t=ticker, c=name_to_use, n=' '.join(name))
+        out_string = "ticker: {t} , company: {n}  ||||||||||| query = {tick} OR ({c}) \n\n".format(t=ticker, tick=ticker_to_use, c=name_to_use, n=' '.join(name))
         query_file.write(out_string)
     # query = 'aapl OR Apple'         # now define number of total tweets you want, should be greater than max_results_per_query
     # number_of_tweets = 500  # now define output file name
