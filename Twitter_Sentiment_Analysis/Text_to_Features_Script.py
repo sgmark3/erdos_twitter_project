405360441af2dca816d1307b1e74e30b91bf2a73
JoshTawabutr <81669491+JoshTawabutr@users.noreply.github.com>
2021-10-31 20:30:21 -0400

    Add processed tables

--- Twitter_Sentiment_Analysis/Text_to_Features_Script.py
+++ Twitter_Sentiment_Analysis/Text_to_Features_Script.py
@@ -48,8 +48,8 @@ def main(filename):
     df_trivial_tweets = df_tweets.loc[df_tweets["Word_count_Henry08_pos"] == 0].loc[df_tweets["Word_count_Henry08_neg"] == 0].loc[df_tweets["Word_count_LM11_pos"] == 0].loc[df_tweets["Word_count_LM11_neg"] == 0].loc[df_tweets["Word_count_Hagenau13_pos"] == 0].loc[df_tweets["Word_count_Hagenau13_neg"] == 0].loc[df_tweets["News_agencies_names_count"] == 0].loc[df_tweets["Compound_vader"] == 0].loc[df_tweets["Positive_vader"] == 0].loc[df_tweets["Negative_vader"] == 0].loc[df_tweets["Neutral_vader"] == 1]
     df_tweets_shorten = df_tweets.drop(df_trivial_tweets.index).copy()
     df_tweets_notext = df_tweets_shorten.drop(columns=["text"]).copy()
-    df_tweets_notext.to_csv(githubpath + "/Data_Preprocessed/df_" + company_name + "_features_added.csv", index=False)
-    print(company_name + "finished")                  # Comment out if prefer quiet
+    df_tweets_notext.to_csv(githubpath + "df_" + company_name + "_features_added.csv", index=False)
+    print(company_name + " finished")                  # Comment out if prefer quiet
 
   
 def get_keywords(filename: str):
